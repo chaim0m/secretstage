4770b682e569f7041d7ae1498b745fd7a09b4fd3
Chaim Finson <chaim0m@hotmail.com>
2018-03-01 12:41:01 +0200

    new service

---
+++ src/app/models/artist.ts
@@ -0,0 +1,10 @@
+export default class Artist {
+    _id: number;
+    name: string;
+    cover: string;
+    genre: string[];
+    enventType: string[];
+    description: string;
+    socialMedia: string[];
+    linktosongs: string[];
+}
