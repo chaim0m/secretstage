c45b3da3b6baba886cde684020f1f2833892a030
Chaim Finson <chaim0m@hotmail.com>
2018-03-12 12:05:56 +0200

    deployment setup

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/artists', artists);
 
